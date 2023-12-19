901413cdf0c6123230decf229738f5ed24ec4487
Your Name <you@example.com>
2023-12-19 11:01:29 -0300

    add image

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FaUpload, FaImage } from "react-icons/fa";
 
-function Formulario({ botao,eventoDigite,cadastrar,obj,cancelar,remover,alterar}) {
+
+function Formulario({ botao, eventoDigite, cadastrar, obj, cancelar, remover, alterar }) {
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleFileUpload = () => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.multiple = false;
+
+        input.click();
+
+        input.onchange = (event) => {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                    setSelectedImage(reader.result);
+                };
+                reader.readAsDataURL(file);
+                console.log(`Arquivo selecionado: ${file.name}`);
+            }
+        };
+    };
+
+    const viewImage = () => {
+        if (selectedImage) {
+            alert('Exibindo a imagem'); // Pode ser substituído por uma lógica para exibir em um modal, por exemplo
+        } else {
+            alert('Nenhuma imagem selecionada');
+        }
+    };
     return (
         <form>
-            <input type="text" value={obj.tarefa} onChange={eventoDigite} name='tarefa' placeholder="Tarefa" className="form-control"></input>
-            <input type="text" value={obj.status} onChange={eventoDigite} name='status' placeholder="Status" className="form-control"></input>
+            <div style={{ display: 'flex' }}>
+                <input
+                    type="text"
+                    value={obj.tarefa}
+                    onChange={eventoDigite}
+                    name='tarefa'
+                    placeholder="Tarefa"
+                    className="form-control"
+                />
+                <button style={{ marginLeft: 1 }} type="button" onClick={handleFileUpload}>
+                    <FaUpload />
+                </button>
+            </div>
+            <select
+                name="status"
+                value={obj.status}
+                onChange={eventoDigite}
+                className="form-control"
+            >
+                <option value="" disabled hidden>Selecione...</option>
+                <option value="Pendente">Pendente</option>
+                <option value="Finalizada">Finalizada</option>
+
+            </select>
             {
                 botao
                     ?
@@ -14,7 +69,16 @@ function Formulario({ botao,eventoDigite,cadastrar,obj,cancelar,remover,alterar}
                         <input type="button" onClick={alterar} value="Alterar" className="btn btn-warning"></input>
                         <input type="button" onClick={remover} value="Remover" className="btn btn-danger"></input>
                         <input type="button" onClick={cancelar} value="Cancelar" className="btn btn-secondary"></input>
+                        <button type="button" onClick={viewImage} className="btn btn-warning">
+                            Ver imagem <FaImage />
+                        </button>
+                        {selectedImage && (
+                            <div id="image-container">
+                                <img src={selectedImage} alt="Imagem Selecionada" style={{ maxWidth: '100%' }} />
+                            </div>
+                        )}
                     </div>
+
             }
 
         </form>
